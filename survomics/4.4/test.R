1a395584ad2829f7b8b5c8580f670acacccfa035
arraytools <arraytools@gmail.com>
2024-05-16 15:52:56 -0400

    add suromics/4.4/test.R

---
+++ survomics/4.4/test.R
@@ -0,0 +1,24 @@
+# load all libraries used in this tutorial except mlr3
+library("TCGAbiolinks")         # Bioconductor package
+library("SummarizedExperiment") # Bioconductor package
+library("DESeq2")               # Bioconductor package
+library("M3C")                  # Bioconductor package
+library("dplyr")
+library("ggplot2")
+library("survival")
+library("survminer")
+library("glmnet")
+library("plotmo")
+library("grpreg")
+library("SGL")
+library("psbcGroup")
+library("psbcSpeedUp")
+library("BhGLM")
+library("risksetROC")
+library("riskRegression")
+library("peperr")
+library("c060")
+library("rms")
+library("survAUC")
+library("regplot")
+library("httpgd")
